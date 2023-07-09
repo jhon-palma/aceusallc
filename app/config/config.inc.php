acf44b0dac7e84a864ab368c1364ae64c808b780
pyjhonn <jhon-palma@upc.edu.co>
2023-07-09 09:05:05 -0500

    first commit

---
+++ app/config/config.inc.php
@@ -0,0 +1,19 @@
+<?php session_start();
+
+    define('ROOT', '');
+    // define the root path to the frontend folder
+    define('VIEWS', ROOT.'app/views/');
+
+//define('HOSTNAME','localhost');
+
+//define('DB_USERNAME','uq7evgbgy9w4k');
+//define('DB_USERNAME','root');
+
+//define('DB_PASSWORD','1zx|%T3%z%&@');
+//define('DB_PASSWORD','');
+
+// define('DB_NAME','dbikmqsixf8kkp');
+//define('DB_NAME','dbshopmovistary2023m05d16');
+
+
+?>
